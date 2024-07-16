00e22b9472edc8793ac253a54d51eaf0020c9b34
RedietNegash <redietnegash9@gmail.com>
2024-07-16 23:10:55 +0300

    defining the eventreducer function

--- Task.js
+++ Task.js
@@ -16,3 +16,35 @@ let initialDataStore = {
     name: null,
   },
 };
+/*
+The EventReducer function takes the current dataStore and the event object and 
+it retruns a draft or a new version of the datastore using the produce function, inorder to maintain the immutablity in the state updates.
+*/
+
+const EventReducer = (dataStore, event) => {
+  return produce(dataStore, (draft) => {
+    switch (event.type) {
+      case "ADD_TO_CART":
+        // Adding the item from the event payload to the cart
+        draft.cart.push(event.payload);
+        break;
+      case "REMOVE_FROM_CART":
+        // filters the cart array in draft then it removes an item whose id matches event.payload.id
+        draft.cart = draft.cart.filter((item) => item.id !== event.payload.id);
+        break;
+      case "LOGIN_USER":
+        // Update the user state to logged in and set the name
+        draft.user.isLoggedIn = true;
+        draft.user.name = event.payload.name;
+        break;
+      case "LOGOUT_USER":
+        // Update the user state to logged out and clear the name
+        draft.user.isLoggedIn = false;
+        draft.user.name = null;
+        break;
+
+      default:
+        break;
+    }
+  });
+};
